38c8dc558b8a958272be3b9d3369b81a8af9051b
DianaSanchezOrdonez <dianaordonez1998@gmail.com>
2022-02-03 10:47:57 -0500

    Heroku

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import { app } from './server'
 
-const port = process.env.PORT ? +process.env.PORT : 3000
+const port = +process.env.PORT || 3000
 
 const server = app.listen(port, () => {
   console.log(`⚡️[server]: server is running on port ${port}`)
